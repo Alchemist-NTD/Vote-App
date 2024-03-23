fc1ef40a9154c57d5acb9ff9a0cfe94035587e31
duy <nguyenthanhduy030898@gmail.com>
2024-03-23 19:42:37 +0700

    add ansible for jenkins

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,18 +11,18 @@ module "jenkins-server" {
     ssh_key       = var.ssh_key
 }
 
-module "builder-server" {
-    source = "./module/ubuntu-22-04"
-    node_count    = 1
-    node_type     = "builder"
-    node_desc     = "builder-server"
-    node_memory   = 4096
-    node_ip_base  = "192.168.1.4"
-    node_disk_size = "32G"
-    template_name = var.template_name
-    vm_password   = var.vm_password
-    ssh_key       = var.ssh_key
-}
+# module "builder-server" {
+#     source = "./module/ubuntu-22-04"
+#     node_count    = 1
+#     node_type     = "builder"
+#     node_desc     = "builder-server"
+#     node_memory   = 4096
+#     node_ip_base  = "192.168.1.4"
+#     node_disk_size = "32G"
+#     template_name = var.template_name
+#     vm_password   = var.vm_password
+#     ssh_key       = var.ssh_key
+# }
 
 module "sonarqube-server" {
     source = "./module/ubuntu-22-04"
@@ -37,33 +37,33 @@ module "sonarqube-server" {
     ssh_key       = var.ssh_key
 }
 
-module "argocd-server" {
-    source = "./module/ubuntu-22-04"
+# module "argocd-server" {
+#     source = "./module/ubuntu-22-04"
 
-    node_count    = 1
-    node_type     = "argoCD"
-    node_desc     = "argocd-server"
-    node_memory   = 4096
-    node_ip_base  = "192.168.1.6"
-    node_disk_size = "32G"
-    template_name = var.template_name
-    vm_password   = var.vm_password
-    ssh_key       = var.ssh_key
-}
+#     node_count    = 1
+#     node_type     = "argoCD"
+#     node_desc     = "argocd-server"
+#     node_memory   = 4096
+#     node_ip_base  = "192.168.1.6"
+#     node_disk_size = "32G"
+#     template_name = var.template_name
+#     vm_password   = var.vm_password
+#     ssh_key       = var.ssh_key
+# }
 
-module "production-server" {
-    source = "./module/ubuntu-22-04"
+# module "production-server" {
+#     source = "./module/ubuntu-22-04"
 
-    node_count    = 1
-    node_type     = "production"
-    node_desc     = "production-server"
-    node_memory   = 8192
-    node_ip_base  = "192.168.1.6"
-    node_disk_size = "64G"
-    template_name = var.template_name
-    vm_password   = var.vm_password
-    ssh_key       = var.ssh_key
-}
+#     node_count    = 1
+#     node_type     = "production"
+#     node_desc     = "production-server"
+#     node_memory   = 8192
+#     node_ip_base  = "192.168.1.6"
+#     node_disk_size = "64G"
+#     template_name = var.template_name
+#     vm_password   = var.vm_password
+#     ssh_key       = var.ssh_key
+# }
 
 module "ansible-server" {
     source = "./module/ubuntu-22-04"
