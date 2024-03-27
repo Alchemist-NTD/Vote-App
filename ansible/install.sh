4398cfede463c8fcece0d7c88944a74a98da516f
duy <nguyenthanhduy030898@gmail.com>
2024-03-27 15:35:31 +0700

    [ZZ] add jenkins agent ansible

--- ansible/install.sh
+++ ansible/install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-tar -cvf ./roles/remote-ansible/files/ansible.tar ../ansible
 ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook \
     -i ./inventory/hosts.yaml \
-    -e "ansible_port=2223" \
-    -e @vars/proxmox-vars.yaml \
-    remote-playbook.yaml
+    -e @vars/vsphere-vars.yaml \
+    playbook.yaml
---
+++ ansible/roles/jenkins-agent/files/Readme.md
@@ -0,0 +1,4 @@
+## Run command at this current dir to get keys pair
+```
+ssh-keygen -t rsa -m PEM -C "Jenkins agent key" -f "jenkinsAgent_rsa"
+```
